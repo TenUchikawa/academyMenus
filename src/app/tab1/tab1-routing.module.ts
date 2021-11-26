dc20228d99d0fbd2a34eee8ce738a6f6851c4482
Ten Uchikawa <ten.uchikawa@gmail.com>
2021-11-26 21:11:47 +0900

    AddedMenu

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -6,11 +6,28 @@ const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
-  }
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'news',
+        loadChildren: () =>
+          import('../news/news.module').then((m) => m.NewsPageModule),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class Tab1PageRoutingModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  paneEnabled = true;
+  constructor(private menuController: MenuController) {}
+  ionViewWillEnter() {
+    this.paneEnabled = true;
+    this.menuController.enable(true, 'second');
+  }
+  ionViewWillLeave() {
+    this.paneEnabled = false;
+  }
 }
